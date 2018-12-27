c389583d485b082871574033aad9a856b28a18e8
Ankitsood007 <43076281+Ankitsood007@users.noreply.github.com>
2018-12-27 03:54:09 -0800

    Add files via upload

---
+++ todo_list.js
@@ -0,0 +1,17 @@
+$("ul").on("click","li",function(){
+ $(this).toggleClass("completed");
+});
+$("ul").on("click","span",function(event){
+  $(this).parent().fadeOut(500,function(){
+  	$(this).remove();
+  });
+  event.stopPropagation();
+});
+$("input").keypress(function(event){
+	if(event.which===13)
+		{
+			var string=$("input").val();
+			$("ul").append("<li>"+"<span><i class='fas fa-trash'></i></span> " +string+"</li");
+			$("input").val("");
+		}
+});
